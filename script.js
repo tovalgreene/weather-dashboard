42fa5b028e19250fa2f65f8feb2a6029216490aa
Tova Greene <tovalgreene@gmail.com>
2023-07-13 18:58:51 -0400

    Added CSS, updated HTML and JS

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-//API KEY -- db73a07b2486b3f74ea1b415140a0677
-var forecastAPIKey = "db73a07b2486b3f74ea1b415140a0677";
-var forecast;
+var city = `Atlanta`;
+var date = `9/13/22`;
+var temp = `76.62 F`;
+var wind = `8.43 MPH`;
+var humidity = `44%`;
 
-var currentAPIKey = "1a71b98e1ddfb208c0e00950b3f40a9e";
-var current;
+   
+function fetchWeatherData(){
+    var currentAPIKey = "1a71b98e1ddfb208c0e00950b3f40a9e";
+    var APIURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+    
+    return fetch(APIURL)
+    .then(response => {
+       return response.json();
+    })
+}
 
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-
-fetch(queryURL); 
+function displayCurrentWeather(city){
+    var currentWeather = document.getElementById('currentweather');
+    currentWeather.innerHTML = `
+    <h2>${city}</h2>
+    <p>${date}</p>
+    <p>Temp: ${temp}</p>
+    <p>Wind: ${wind}</p>
+    <p>Humidity: ${humidity}</p>
+    `;
+}
