cf458a9cfab19ba28e03b63197aa91460bc14186
Tova Greene <tovalgreene@gmail.com>
2023-10-01 20:17:00 -0400

    edited html, readme, js, and css

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# weather-dashboard
+# Weather Dashboard
 
-User Story
+## Description
+The Weather Dashboard is a dynamic web application that allows users to view the current and future weather conditions of multiple cities. It provides users with real-time weather updates, including a 5-day forecast, leveraging the OpenWeatherMap API to retrieve accurate weather data.
+
+## User Story
 AS A traveler
 I WANT to see the weather outlook for multiple cities
 SO THAT I can plan a trip accordingly
 
-Acceptance Criteria
+## Acceptance Criteria
 GIVEN a weather dashboard with form inputs
 WHEN I search for a city
 THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -15,3 +18,28 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
+
+## Features
+- Real-time weather updates.
+- 5-day weather forecast.
+- Interactive user interface with search functionality.
+- Persistent data storage for search history.
+
+## Technologies Used
+- HTML5
+- CSS3
+- JavaScript
+- OpenWeatherMap API
+
+## Deployment
+The Weather Dashboard is deployed here: .
+
+## Repository
+- Project Repo URL:
+
+## Credits
+- OpenWeatherMap API: https://openweathermap.org/api
+
+## License
+This project is licensed under the MIT License.
+
--- script.js
+++ script.js
@@ -1,30 +1,99 @@
-var city = `Atlanta`;
-var date = `9/13/22`;
-var temp = `76.62 F`;
-var wind = `8.43 MPH`;
-var humidity = `44%`;
+// Declare API Key
+var currentAPIKey = "1a71b98e1ddfb208c0e00950b3f40a9e";
+var forecastAPIKey = "db73a07b2486b3f74ea1b415140a0677";
 
-   
-function fetchWeatherData(){
-    var currentAPIKey = "1a71b98e1ddfb208c0e00950b3f40a9e";
-    var APIURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+// Function to fetch current weather data for a city
+function fetchCurrentWeather(city) {
+    var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${currentAPIKey}&units=imperial`;
     
-    return fetch(APIURL)
+    return fetch(currentWeatherURL)
     .then(response => {
-       return response.json();
-    })
+        if(response.ok) return response.json();
+        throw new Error('Failed to fetch current weather data');
+    });
 }
 
-function displayCurrentWeather(city){
+// Function to fetch 5-day forecast data for a city
+function fetchFiveDayForecast(city) {
+    var fiveDayForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${forecastAPIKey}&units=imperial`;
+    
+    return fetch(fiveDayForecastURL)
+    .then(response => {
+        if(response.ok) return response.json();
+        throw new Error('Failed to fetch five day forecast data');
+    });
+}
+
+// Function to display current weather data on the webpage
+function displayCurrentWeather(data) {
     var currentWeather = document.getElementById('currentweather');
+    // Format and display the fetched data
     currentWeather.innerHTML = `
-    <h2>${city}</h2>
-    <p>${date}</p>
-    <p>Temp: ${temp}</p>
-    <p>Wind: ${wind}</p>
-    <p>Humidity: ${humidity}</p>
+        <h2>${data.name}</h2>
+        <p>${new Date().toLocaleDateString()}</p>
+        <p>Temp: ${data.main.temp} F</p>
+        <p>Wind: ${data.wind.speed} MPH</p>
+        <p>Humidity: ${data.main.humidity}%</p>
     `;
 }
 
-var search = document.getElementById('search');
-search.addEventListener('searchbutton');
+function displayFiveDayForecast(data) {
+    var fiveDayForecast = document.getElementById('5dayforecast');
+    var forecastList = data.list;
+    
+    // Initialize the innerHTML to an empty string
+    fiveDayForecast.innerHTML = '';
+    
+    for(let i = 0; i < forecastList.length; i += 8) { // Here we are skipping every 8th record to get one record per day
+        var dailyForecast = forecastList[i];
+        
+        // Create elements to hold the forecast data
+        var forecastDiv = document.createElement('div');
+        forecastDiv.className = 'forecast';
+
+        var dateEl = document.createElement('p');
+        dateEl.textContent = new Date(dailyForecast.dt * 1000).toLocaleDateString(); // Convert timestamp to date string
+        forecastDiv.appendChild(dateEl);
+
+        var iconEl = document.createElement('img');
+        iconEl.src = `http://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}.png`; // Use the icon code to get the icon URL
+        forecastDiv.appendChild(iconEl);
+
+        var tempEl = document.createElement('p');
+        tempEl.textContent = `Temp: ${dailyForecast.main.temp} F`;
+        forecastDiv.appendChild(tempEl);
+
+        var windEl = document.createElement('p');
+        windEl.textContent = `Wind: ${dailyForecast.wind.speed} MPH`;
+        forecastDiv.appendChild(windEl);
+
+        var humidityEl = document.createElement('p');
+        humidityEl.textContent = `Humidity: ${dailyForecast.main.humidity}%`;
+        forecastDiv.appendChild(humidityEl);
+        
+        // Append the forecastDiv to the fiveDayForecast container
+        fiveDayForecast.appendChild(forecastDiv);
+    }
+}
+
+function handleSearch() {
+    var searchInput = document.getElementById('city');
+    var city = searchInput.value.trim();
+    if(city) {
+        searchInput.value = ''; // clear the input value
+        fetchCurrentWeather(city)
+        .then(data => {
+            displayCurrentWeather(data);
+            return fetchFiveDayForecast(city);
+        })
+        .then(data => {
+            displayFiveDayForecast(data);
+        })
+        .catch(error => {
+            console.error("Error fetching weather data: ", error);
+        });
+    }
+}
+
+var searchButton = document.getElementById('searchbutton');
+searchButton.addEventListener('click', handleSearch);
